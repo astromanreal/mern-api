971fdf7cae09f7a7efc957c0d8fb1cfbca0c7049
Manish Pandey <137053707+astromanreal@users.noreply.github.com>
2024-01-12 01:37:57 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const ConnectDB = () => {
   try {
-    mongoose.connect(process.env.MONGO_URI);
+    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("DB connected!");
   } catch (error) {
     console.log("ERROR in Database");
